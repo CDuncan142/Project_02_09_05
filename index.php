c79d4f7464d65530a1e7ce82ac14d2171069acdc
CDuncan142 <CDuncan142@West-MEC.org>
2018-12-05 16:56:13 -0700

    index update

--- index.php
+++ index.php
@@ -1,11 +1,26 @@
-<!--
-Author: Conner Duncan
-Date: 12.04.18
+<?php
+session_start();
+echo "Session ID: " . session_id() . "<br />\n";
+?>
+<!doctype html>
+<html>
+	<head>
+		<!--
+		Author: Conner Duncan
+		Date: 12.04.18
 
-file: index.php
--->
+		file: index.php
+		-->
+		<title>Index.php</title>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="initial-scale=1.0">
+	</head>
 
-<!--session-->
+	<body>
+
+	</body>
+</html>
+
 <?php
 
 
